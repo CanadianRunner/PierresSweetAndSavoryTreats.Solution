4b787a6926ed2396c6c9217b45978711e3f0a7d5
Sean Keane <seankeane93@gmail.com>
2022-08-25 12:41:19 -0700

    WIP developing logic for the TreatsController.cs

--- Bakery/Controllers/HomeController.cs
+++ Bakery/Controllers/HomeController.cs
@@ -1,37 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
 using Bakery.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Bakery.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+      private readonly BakeryContext _db;
+      public HomeController(BakeryContext db)
+      {
+        _db = db;
+      }
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        List<Treat> treatList = _db.Treats.ToList();
+        ViewBag.Flavours = _db.Flavours.ToList();
+        return View();
+      }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
