cdb32f9e50fd90a5ff5782eff1469dce3ad98a65
Sean Keane <seankeane93@gmail.com>
2022-08-25 15:04:58 -0700

    Developed logic for Flavour Model

---
+++ Bakery/Models/Flavour.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+{
+  public class Flavour
+  {
+    public Flavour()
+    {
+      this.JoinEntities = new HashSet<FlavourTreat>();
+    }
+
+    public int FlavourId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public virtual UserId User { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
+  }
+}
